ef141f0d72c3632e7e1b4e48a1ff4788a7517eda
Rinat <rinatm@novarize.com>
2020-06-08 18:14:35 +0300

    add sqlite and jooq

---
+++ app/Dao/Device.scala
@@ -0,0 +1,44 @@
+package Dao
+
+import entities.DeviceInfo
+import org.jooq.impl.DSL
+import org.jooq.{Record, Result}
+
+import scala.jdk.javaapi.CollectionConverters.asScala
+
+
+class Device extends Dao {
+
+  private val tableName = "Devices"
+
+  def getDeviceInfo(userAgent: String): Option[DeviceInfo] = {
+    val devicesInfo = dslContext.selectFrom(tableName)
+      .where(DSL.field("user_agent").equal(userAgent))
+      .fetch()
+    mapper(devicesInfo).headOption
+  }
+
+  def insertDeviceInfo(deviceInfo: DeviceInfo) = {
+    dslContext.insertInto(DSL.table(tableName))
+      .columns(DSL.field("user_agent"), DSL.field("is_mobile_device"), DSL.field("type"), DSL.field("brand"),
+        DSL.field("brand_code"), DSL.field("brand_url"), DSL.field("name"))
+      .values(deviceInfo.userAgent.get, deviceInfo.isMobileDevice, deviceInfo.deviceType, deviceInfo.brand,
+        deviceInfo.brandCode, deviceInfo.brandUrl, deviceInfo.name)
+      .execute()
+
+  }
+
+  private def mapper(res: Result[Record]): Seq[DeviceInfo] = {
+    asScala(res.map { r =>
+      val userAgent = r.getValue("user_agent").toString
+      val isMobileDevice = r.getValue("is_mobile_device").toString
+      val deviceType = r.getValue("type").toString
+      val brand = r.getValue("brand").toString
+      val brandCode = r.getValue("brand_code").toString
+      val brandUrl = r.getValue("brand_url").toString
+      val name = r.getValue("name").toString
+      DeviceInfo(isMobileDevice, deviceType, brand, brandCode, brandUrl, name, Some(userAgent))
+    }).toSeq
+  }
+}
+
--- build.sbt
+++ build.sbt
@@ -9,7 +9,11 @@ lazy val root = (project in file("."))
       "com.h2database" % "h2" % "1.4.199",
       "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test,
       ws,
-      "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.1.2"
+      "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.1.2",
+      "org.jooq" % "jooq" % "3.13.2",
+      "org.jooq" % "jooq-meta" % "3.13.2",
+      "org.jooq" % "jooq-codegen" % "3.13.2",
+      "org.xerial" % "sqlite-jdbc" % "3.31.1"
     ),
     scalacOptions ++= Seq(
       "-feature",
