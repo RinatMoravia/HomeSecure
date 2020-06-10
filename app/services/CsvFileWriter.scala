d39ba2660378fcb1e10beb4a5b169de1908894aa
Rinat <rinatm@novarize.com>
2020-06-10 19:26:35 +0300

    add file writer

---
+++ app/services/CsvFileWriter.scala
@@ -0,0 +1,19 @@
+package services
+
+import java.io.{BufferedWriter, FileWriter}
+
+import au.com.bytecode.opencsv.CSVWriter
+
+
+case class CsvFileWriter(fileName: String){
+
+  private val outputFile = new BufferedWriter(new FileWriter(fileName))
+  private val csvWriter = new CSVWriter(outputFile)
+
+  def writeNextLine(string: String*): Unit = {
+    csvWriter.writeNext(string.toArray:_*)
+  }
+
+  def closeFile(): Unit = outputFile.close()
+
+}
--- app/src/Main.scala
+++ app/src/Main.scala
@@ -16,35 +16,10 @@ import scala.collection.mutable.ListBuffer
 
 object Main extends App {
 
-//  if (args.length != 1){
-//    println("bad input")
-//    System.exit(1)
-//  }
-//  val injector = Guice.createInjector(new Module)
-//  val apiService = injector.getInstance(classOf[ApiService])
-//  val deviceDao = injector.getInstance(classOf[DeviceDao])
-
-
-//  apiService.getDevicesDetails("Xbox Live Client/2.0.17511.0")
-//"/Users/rinatmoravia/Downloads/test/devices.csv"
-
-  val outputFile = new BufferedWriter(new FileWriter("/Users/rinatmoravia/homesecure.csv"))
-  val csvWriter = new CSVWriter(outputFile)
-  val csvSchema = Array("id", "name", "age", "city")
-  //("id", "name", "age", "city")
-  val records =  Array("3000", "rinat", "33", "RoshHaifg")
-  var listOfRecords = new ListBuffer[Array[String]]()
-  listOfRecords += csvSchema
-//  listOfRecords += records
-
-val myStr = "300,fdgfg,dfgf,gggg"
-//  csvWriter.writeAll(asJava(listOfRecords))
-  csvWriter.writeNext(myStr)
-  outputFile.close()
-
-
-
-
+  if (args.length != 1){
+    println("bad input")
+    System.exit(1)
+  }
 
 
 
